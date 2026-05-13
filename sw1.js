61294dd3ad819c05eb56b2a3c3d908bf65ad81bc
kartikguptadarsh <vikashkrgupta1982@gmail.com>
2026-05-13 20:24:52 +0530

    Update notification icon and badge URLs

--- sw1.js
+++ sw1.js
@@ -2,8 +2,9 @@ self.addEventListener('push', function(event) {
     const data = event.data.json();
     const options = {
         body: data.message,
-        icon: 'icon.png', // Replace with your icon if you have one
-        badge: 'icon.png'
+        icon: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
+        vibrate: [200, 100, 200]
     };
     event.waitUntil(self.registration.showNotification(data.title, options));
 });
